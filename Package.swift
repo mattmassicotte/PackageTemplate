6f805b37809e3a88d2ddeb39083eb6c6e3fafa03
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-08-31 06:49:50 -0400

    package structure

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.8
+
+import PackageDescription
+
+let settings: [SwiftSetting] = [
+    .enableExperimentalFeature("StrictConcurrency")
+]
+
+let package = Package(
+    name: "Template",
+    products: [
+        .library(name: "Template", targets: ["Template"]),
+    ],
+    targets: [
+        .target(
+            name: "Template",
+            swiftSettings: settings
+        ),
+        .testTarget(
+            name: "TemplateTests",
+            dependencies: ["Template"],
+            swiftSettings: settings
+        ),
+    ]
+)
